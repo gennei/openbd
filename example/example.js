60f9bc57c5f394d7e7702a272f0be98aac41eb5a
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-08 15:09:04 +0900

    replace code "request" -> "axios"

--- example/example.js
+++ example/example.js
@@ -4,13 +4,20 @@ let openbd = new openBD();
 /*
  * 1件だけ取得
  */
-openbd.get('4088810023', function(err, response, body) {
-  console.log(body);
+openbd.get('4088810023').then(res=>{
+  console.log(res.data);
 });
 
 /*
  * 複数件取得
  */
-openbd.get(new Array('4088810023', '4088725093'), function(err, response, body) {
-  console.log(body);
+openbd.get(new Array('4088810023', '4088725093')).then(res=>{
+  console.log(res.data);
+  console.log(res.data.length);
+});
+/**
+ * 全件ISBN
+ */
+openbd.coverage().then(res=>{
+  console.log(res.data)
 });
